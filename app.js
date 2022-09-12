cbb2c1b5c0754838e7ef41f1cd4e6b20f56947d7
DamianCenco <damian.cancela@cencosud.com>
2022-09-12 12:19:42 -0300

    Rutas y archivos ejs

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const app = express();
+const port = 3000;
+const products = require("./data/products.json");
 
-// app.get("/", (req, res)=> 
-// {
-//     res.send("test routes")
-// })
+app.use(express.static("public"));
 
-app.listen(3000, console.log("listen on port 3000"))
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/product/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = products.find((product) => product.id === productId);
+  product !== undefined
+    ? res.render("product", { product })
+    : res.send("No se encuentra producto");
+});
+
+app.get("/cart", (req, res) => {
+  const productsInCart = products.splice(0, 2);
+  res.render("cart", { productsInCart });
+});
+
+app.listen(port, console.log(`listen on port ${3000}`));
