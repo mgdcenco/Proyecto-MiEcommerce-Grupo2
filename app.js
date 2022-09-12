2f68ae8ef381d6434a70a32de579106452d43976
facundovacanicencosud <facundo.vacani@cencosud.com>
2022-09-12 18:26:57 -0300

    agregando la pag de error y arreglando algunas cosas de products detail

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.get("/product/:id", (req, res) => {
   let product = products.find((prod) => prod.id === productId);
   console.log(product);
   product !== undefined
-    ? res.render("product", { product, url })
-    : res.render("product", {error:true, url});
+    ? res.render("product", { product, url, error:false })
+    : res.render("product", { url, error:true});
 });
 
 app.get("/cart", (req, res) => {
