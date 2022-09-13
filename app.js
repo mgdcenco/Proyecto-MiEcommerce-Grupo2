125bb7b86a95be38a541aefd41ad5fd94632dabd
DamianCenco <damian.cancela@cencosud.com>
2022-09-13 11:45:39 -0300

    Productos dinamicos, navbar fixed, fixes en estilos

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ app.set("views", "./src/views/pages");
 
 app.set("view engine", "ejs");
 
-
 app.get("/", (req, res) => {
   const url = req.url;
-  res.render("index", { url });
+  res.render("index", { url, products });
 });
 
 app.get("/login", (req, res) => {
@@ -24,26 +23,22 @@ app.get("/register", (req, res) => {
   res.render("register", { url });
 });
 
-app.get("/register", (req, res) => {
-  res.render("./pages/register");
-});
-
 app.get("/product/:id", (req, res) => {
   const productId = parseInt(req.params.id);
   let product = products.find((prod) => prod.id === productId);
   const url = req.url;
   product !== undefined
-  ? res.render("product", { product, url, error:false })
-  : res.render("product", { url, error:true});
+    ? res.render("product", { product, url, error: false, products })
+    : res.render("product", { url, error: true });
 });
 
 app.get("/cart", (req, res) => {
   const url = req.url;
-  let numRandom = Math.round(Math.random() * products.length -3);
-  if(numRandom <= 0){
+  let numRandom = Math.round(Math.random() * products.length - 3);
+  if (numRandom <= 0) {
     numRandom = 1;
   }
-  const productsInCart = products.slice(numRandom, numRandom+3);
+  const productsInCart = products.slice(numRandom, numRandom + 3);
   res.render("cart", { products: productsInCart, url });
 });
 
