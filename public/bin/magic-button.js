398161146d129abec79c35970138b6ae366a4c95
facundovacanicencosud <facundo.vacani@cencosud.com>
2022-09-19 18:12:58 -0300

    haciendo arreglos al js de los botones 404 general

--- public/bin/magic-button.js
+++ public/bin/magic-button.js
@@ -1,6 +1,7 @@
 const redirect_magic_icon = document.querySelector(".magic-redirect-icon");
 const magic_button = document.querySelector("#magic-button");
 const random = Math.round(Math.random() * 30);
+const buttonHome = document.querySelector(".section-error--button");
 
 if (magic_button) {
   magic_button.addEventListener("click", () => {
@@ -10,4 +11,10 @@ if (magic_button) {
       window.location.replace(`/product/${random}`);
     }, 1000);
   });
+}
+
+if(buttonHome){
+  buttonHome.addEventListener("click", ()=>{
+    window.location.replace("/")
+  })
 }
