9e2ca6a7021621e1c60586c3eaddaf0a55eb90bb
facundovacanicencosud <facundo.vacani@cencosud.com>
2022-09-20 16:46:11 -0300

    agregando el path a control category

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -7,12 +7,13 @@ module.exports = {
 
             let category = req.params.category;
             const products = await getProducts();
+            let path = req.path;
             const productsByCategory = products.filter((pro) => pro.category == category);
             let noSearchBox = false;    
             if(productsByCategory.length > 0){
                 res.render("category", {products: productsByCategory, noSearchBox, category})
             }else{
-                res.render("error404", {noSearchBox, products})
+                res.render("error404", {noSearchBox, products, path})
             }
         }catch(err){
             console.log(err);
