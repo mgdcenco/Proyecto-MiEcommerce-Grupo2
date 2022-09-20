f8751a089bc9c081f517c2762f3bc2528e09c815
facundovacanicencosud <facundo.vacani@cencosud.com>
2022-09-20 11:22:56 -0300

    Ordenando por mayor rate en el index

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -6,8 +6,9 @@ module.exports = {
         try{
             const products = await getProducts();
             let noSearchBox = false;
+            const rateProducts = [...products].sort((prod1,prod2)=> prod2.rating.rate - prod1.rating.rate);
             const mostPopular = [...products].sort((product1, product2)=> product2.rating.count - product1.rating.count);
-            res.render("index", {products, mostPopular, noSearchBox });
+            res.render("index", {products: rateProducts, mostPopular, noSearchBox });
         }catch (err){
             console.log(err);
         }
