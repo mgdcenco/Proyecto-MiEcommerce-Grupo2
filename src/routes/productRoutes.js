30a3bdf0f9d51d2936021eae7a13d2848907e85e
DamianCenco <damian.cancela@cencosud.com>
2022-09-20 16:30:26 -0300

    easter egg

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
+router.get("/product/fer", productController.showFer);
 router.get("/product/:id", productController.showProduct);
 router.get("/products/search", productController.searchProducts);
 
