6898101fe5246ed46bc116023401d3154f15bbcf
facundovacanicencosud <facundo.vacani@cencosud.com>
2022-09-26 11:25:09 -0300

    terminando user story 08 sprint 3

--- public/bin/product-detail.js
+++ public/bin/product-detail.js
@@ -3,11 +3,26 @@ const primaryImage = document.querySelector(".section-product-detail__images-pri
 if(primaryImage){
     const collectionImages = document.querySelectorAll(".section-product-detail__images-collection-img");
 
-
-    collectionImages.forEach((elem) => {
+    let primaryImageConst = primaryImage.src;
+    collectionImages[0].classList.add("image-product-active");
+    collectionImages.forEach((elem,index) => {
         elem.addEventListener("mouseover", ()=>{
             primaryImage.src = elem.src;
-        })
+        });
+        elem.addEventListener("mouseleave", ()=>{
+            primaryImage.src = primaryImageConst;
+        });
+        elem.addEventListener("click", ()=>{
+            primaryImageConst = elem.src;
+            primaryImage.src = elem.src;
+            collectionImages[0].classList.remove("image-product-active")
+            elem.classList.add("image-product-active");
+            for(let i = 0; i < collectionImages.length; i++){
+                if(i !== index){
+                    collectionImages[i].classList.remove("image-product-active");
+                }
+            }
+        });
     })
 
 }
