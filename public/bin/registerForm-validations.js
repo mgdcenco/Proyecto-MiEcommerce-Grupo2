528c6397b620bcb6bb734329f6a7c74468037669
mgdcenco <martin.gamboa@cencosud.com>
2022-09-26 16:50:35 -0300

    adding more validations, email provider is evaluated to true

--- public/bin/registerForm-validations.js
+++ public/bin/registerForm-validations.js
@@ -1,44 +1,45 @@
 let registerForm = document.querySelector("#registerForm");
 let registerFormName = document.querySelector("#name");
 let registerFormLastname = document.querySelector("#lastname");
-let registerFormUsernameInput = document.querySelector("#username");
-let registerFormPasswordInput = document.querySelector("#password");
-let registerFormRepeatedPasswordInput =
+let registerFormUsername = document.querySelector("#username");
+let registerFormPassword = document.querySelector("#password");
+let registerFormRepeatedPassword =
   document.querySelector("#passwordRepeated");
 let registerFormButton = document.querySelector("#registerFormButton");
 
 if (registerFormButton) {
-    let nameLength = 0;
-    let lastnameLength = 0;
-    let usernameLength = 0;
-    let passwordLength = 0;
-    let passwordRepeatedLength = 0;
+    let name ;
+    let lastname;
+    let username;
+    let password;
+    let passwordRepeated ;
 
-  registerForm.addEventListener("keyup", (e) => {
+  registerForm.addEventListener("keyup", () => {
 
-    if(e.target.id == "username"){
-        usernameLength = e.target.value.length
-    }
-    else if(e.target.id == "password"){
-        passwordLength = e.target.value.length
-    }
-    else if(e.target.id == "passwordRepeated"){
-        passwordRepeatedLength = e.target.value.length
-    }
-    else if(e.target.id == "name"){
-        nameLength = e.target.value.length
-    }
-    else if(e.target.id == "lastname"){
-        lastnameLength = e.target.value.length
+    name = registerFormName.value;
+    lastname = registerFormLastname.value;
+    username = registerFormUsername.value;
+    password = registerFormPassword.value;
+    passwordRepeated = registerFormRepeatedPassword.value;
+    validProvider = false;
+    
+    let atSign = username.lastIndexOf('@') + 1;
+    let dot = username.lastIndexOf('.');
+    let emailProvider = username.slice(atSign, dot)
+
+    if (emailProvider == "gmail" || emailProvider == "yahoo" || emailProvider == "outlook" || emailProvider == "hotmail"){
+        validProvider = true;
     }
 
-    if (nameLength != 0 && lastnameLength != 0 && usernameLength != 0 && passwordLength != 0 && passwordRepeatedLength != 0) {
+    console.log(validProvider);
+
+    if (name != "" && lastname != "" && password != "" && passwordRepeated != "" && password.length >= 8 && password == passwordRepeated && validProvider) {
         console.log("Boton habilitado");
         registerFormButton.removeAttribute("disabled");
         registerFormButton.classList.remove("formRegister--submit-button-inactive");
         registerFormButton.classList.add("formRegister--submit-button-active");
     }
-    else if(nameLength == 0 || lastnameLength == 0 || usernameLength == 0 || passwordLength == 0 || passwordRepeatedLength == 0){
+    else if(name == "" || lastname == "" || username == "" || password == "" || passwordRepeated == "" || !validProvider){
         console.log("Boton inhabilitado");
         registerFormButton.setAttribute("disabled", "");
         registerFormButton.classList.add("formRegister--submit-button-inactive");
