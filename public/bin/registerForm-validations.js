4f2878f215e943c27e31a21080ee2979433e12e4
mgdcenco <martin.gamboa@cencosud.com>
2022-09-26 19:08:19 -0300

    Refactoring register form validation code, testing svg with alert logo in front

--- public/bin/registerForm-validations.js
+++ public/bin/registerForm-validations.js
@@ -12,7 +12,7 @@ if (registerFormButton) {
     let lastname;
     let username;
     let password;
-    let passwordRepeated ;
+    let repeatedPassword ;
 
   registerForm.addEventListener("keyup", () => {
 
@@ -20,27 +20,62 @@ if (registerFormButton) {
     lastname = registerFormLastname.value;
     username = registerFormUsername.value;
     password = registerFormPassword.value;
-    passwordRepeated = registerFormRepeatedPassword.value;
-    validProvider = false;
+    repeatedPassword = registerFormRepeatedPassword.value;
+
+    let validName = false;
+    let validLastname = false;
+    let validUsername = false;
+    let validPassword = false;
+    let validRepeatedPassword = false;
+    let validProvider = false;
     
     let atSign = username.lastIndexOf('@'); // Obtiene la posición del @ en el email del usuario
     let emailProvider = username.slice(atSign); // Obtiene el proveedor de email, ej test@gmail.com devuelve @gmail.com.
     let emailName = username.slice(0, atSign); // Obtiene el nombre del nombre, siguiendo el ejemplo anterior devuelve test
 
+    if(name != "" && name[0] != " " && name[name.length - 1] != " "){
+        validName = true;
+    }
+    if(lastname != "" && lastname[0] != " " && lastname[lastname.length - 1] != " "){
+        validLastname = true;
+    }
+    if(emailName != ""){
+        validUsername = true;
+    }
     if (emailProvider == "@gmail.com" && emailName != " "|| emailProvider == "@yahoo.com" || emailProvider == "@outlook.com"){
         validProvider = true;
     }
+    if(password != "" && password.length >= 8){
+        validPassword = true;
+    }
+    if(repeatedPassword != "" && password == repeatedPassword){
+        validRepeatedPassword = true
+    }
 
-    if (name != "" && name[0] != " " && name[name.length - 1] != " " && lastname != "" && lastname[0] != " " && lastname[lastname.length - 1] != " "  && password != "" && passwordRepeated != "" && password.length >= 8 && password == passwordRepeated && validProvider && emailName != "") {
+    // if (name != "" && name[0] != " " && name[name.length - 1] != " " && lastname != "" && lastname[0] != " " && lastname[lastname.length - 1] != " "  && password != "" && passwordRepeated != "" && password.length >= 8 && password == passwordRepeated && validProvider && emailName != "") {
+    //     registerFormButton.removeAttribute("disabled");
+    //     registerFormButton.classList.remove("formRegister--submit-button-inactive");
+    //     registerFormButton.classList.add("formRegister--submit-button-active");
+    // }
+
+    // console.log(`validName = ${validName}`);
+    // console.log(`validLastname = ${validLastname}`);
+    // console.log(`validUsername = ${validUsername}`);
+    // console.log(`validProvider = ${validProvider}`);
+    // console.log(`validPassword = ${validPassword}`);
+    // console.log(`validPasswordRepeated = ${validRepeatedPassword}`);
+
+    if(validName && validLastname && validUsername && validProvider && validPassword && validRepeatedPassword){
         registerFormButton.removeAttribute("disabled");
         registerFormButton.classList.remove("formRegister--submit-button-inactive");
         registerFormButton.classList.add("formRegister--submit-button-active");
     }
-    else if(name == " " || name[0] == " " || name[name.length - 1] == " " || lastname == "" || lastname[0] == " " || lastname[lastname.length - 1] == " " || username == "" || password == "" || passwordRepeated == "" || !validProvider || password != passwordRepeated || emailName == ""){
-        registerFormButton.setAttribute("disabled", "");
-        registerFormButton.classList.add("formRegister--submit-button-inactive");
-        registerFormButton.classList.remove("formRegister--submit-button-active");
-    }
+
+    // else if(name == " " || name[0] == " " || name[name.length - 1] == " " || lastname == "" || lastname[0] == " " || lastname[lastname.length - 1] == " " || username == "" || password == "" || passwordRepeated == "" || !validProvider || password != passwordRepeated || emailName == ""){
+    //     registerFormButton.setAttribute("disabled", "");
+    //     registerFormButton.classList.add("formRegister--submit-button-inactive");
+    //     registerFormButton.classList.remove("formRegister--submit-button-active");
+    // }
 
   });
 }
