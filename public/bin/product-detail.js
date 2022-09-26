c45ce9b95dd33dbd5451d7f6749336f2274be1ce
facundovacanicencosud <facundo.vacani@cencosud.com>
2022-09-26 15:10:09 -0300

    agregando mouseover al padre de las img

--- public/bin/product-detail.js
+++ public/bin/product-detail.js
@@ -1,6 +1,9 @@
 const primaryImage = document.querySelector(".section-product-detail__images-primary");
 
 if(primaryImage){
+    //array de imagenes
+    const collectionImagesAll = document.querySelector(".section-product-detail__images-collection");
+    //el div que contiene las imagenes
     const collectionImages = document.querySelectorAll(".section-product-detail__images-collection-img");
 
     let primaryImageConst = primaryImage.src;
@@ -9,13 +12,13 @@ if(primaryImage){
         elem.addEventListener("mouseover", ()=>{
             primaryImage.src = elem.src;
         });
-        elem.addEventListener("mouseleave", ()=>{
+        //evento mouseleave al div padre de las imagenes, ya que al dejarla en la imagen hija, entre imagen e imagen ,se va a notar la imagen principal. De esta forma se ve mejor
+        collectionImagesAll.addEventListener("mouseleave", ()=>{
             primaryImage.src = primaryImageConst;
         });
         elem.addEventListener("click", ()=>{
             primaryImageConst = elem.src;
             primaryImage.src = elem.src;
-            collectionImages[0].classList.remove("image-product-active")
             elem.classList.add("image-product-active");
             for(let i = 0; i < collectionImages.length; i++){
                 if(i !== index){
