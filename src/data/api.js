b31ac2f9cb975131139714bb02c56bd5a2666dd1
FernandoEtcheverry <francisco.etcheverry@cencosud>
2022-09-26 16:32:26 -0300

    js carrito

--- src/data/api.js
+++ src/data/api.js
@@ -1,24 +1,31 @@
 const fetch = require("node-fetch");
 module.exports = {
-    getProducts: async () => {
-      try{
-        const response = await fetch('http://localhost:5000/api/product');
-        const json = await response.json();
-        return json
-
-      }catch (err){
-        console.log(err);
-      }
-    },
-    getProductById: async (id) => {
-      try{
-        const response = await fetch(`http://localhost:5000/api/product/${id}`);
-        const json = await response.json();
-        return json
-        
-      }catch(err){
-        console.log(err);
-      }
+  getProducts: async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/product");
+      const json = await response.json();
+      return json;
+    } catch (err) {
+      console.log(err);
     }
+  },
+  getProductById: async (id) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/product/${id}`);
+      const json = await response.json();
+      return json;
+    } catch (err) {
+      console.log(err);
+    }
+  },
 
-}
+  getCartByUser: async (id) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/cart/${id}`);
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
