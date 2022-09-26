e4bb0c5707f15790a42072f22927970899c186e3
mgdcenco <martin.gamboa@cencosud.com>
2022-09-26 14:59:16 -0300

    fixing registercss, adding ids and classes, adding Name and LastName inputs, first validation running

--- public/bin/registerForm-validations.js
+++ public/bin/registerForm-validations.js
@@ -1,4 +1,6 @@
 let registerForm = document.querySelector("#registerForm");
+let registerFormName = document.querySelector("#name");
+let registerFormLastname = document.querySelector("#lastname");
 let registerFormUsernameInput = document.querySelector("#username");
 let registerFormPasswordInput = document.querySelector("#password");
 let registerFormRepeatedPasswordInput =
@@ -6,7 +8,8 @@ let registerFormRepeatedPasswordInput =
 let registerFormButton = document.querySelector("#registerFormButton");
 
 if (registerFormButton) {
-
+    let nameLength = 0;
+    let lastnameLength = 0;
     let usernameLength = 0;
     let passwordLength = 0;
     let passwordRepeatedLength = 0;
@@ -15,24 +18,27 @@ if (registerFormButton) {
 
     if(e.target.id == "username"){
         usernameLength = e.target.value.length
-        console.log(`username lenght = ${usernameLength}`);
     }
     else if(e.target.id == "password"){
         passwordLength = e.target.value.length
-        console.log(`password lenght = ${passwordLength}`);
     }
     else if(e.target.id == "passwordRepeated"){
         passwordRepeatedLength = e.target.value.length
-        console.log(`passwordRepeated lenght = ${passwordRepeatedLength}`);
+    }
+    else if(e.target.id == "name"){
+        nameLength = e.target.value.length
+    }
+    else if(e.target.id == "lastname"){
+        lastnameLength = e.target.value.length
     }
 
-    if (usernameLength != 0 && passwordLength != 0 && passwordRepeatedLength != 0) {
+    if (nameLength != 0 && lastnameLength != 0 && usernameLength != 0 && passwordLength != 0 && passwordRepeatedLength != 0) {
         console.log("Boton habilitado");
         registerFormButton.removeAttribute("disabled");
         registerFormButton.classList.remove("formRegister--submit-button-inactive");
         registerFormButton.classList.add("formRegister--submit-button-active");
     }
-    else if(usernameLength == 0 || passwordLength == 0 || passwordRepeatedLength == 0){
+    else if(nameLength == 0 || lastnameLength == 0 || usernameLength == 0 || passwordLength == 0 || passwordRepeatedLength == 0){
         console.log("Boton inhabilitado");
         registerFormButton.setAttribute("disabled", "");
         registerFormButton.classList.add("formRegister--submit-button-inactive");
