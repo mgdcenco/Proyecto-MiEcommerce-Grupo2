b9a083aba33f57a426a317e1aa10cdda0f9f87a7
DamianCenco <damian.cancela@cencosud.com>
2022-09-28 00:23:13 -0300

    Arreglando estilos del form

--- public/bin/dark-mode.js
+++ public/bin/dark-mode.js
@@ -1,7 +1,8 @@
 let darkModeBtn = document.querySelectorAll(".dark-mode-btn");
 let body = document.querySelector("body");
 let productCards = document.querySelectorAll(".product-list--cards--card");
-let productsInCart = document.querySelectorAll(".product-in-cart--dark-themed");
+let productsInCart = document.querySelectorAll(".product-in-cart");
+console.log(productsInCart);
 
 let theme = localStorage.getItem("Theme");
 window.addEventListener("load", function (e) {
@@ -10,6 +11,9 @@ window.addEventListener("load", function (e) {
     body.classList.toggle("dark-mode--body");
     productCards.forEach((product) =>
       product.classList.toggle("product-list--cards--card--dark-theme")
+    );
+    productsInCart.forEach((product) =>
+      product.classList.toggle("product-in-cart--dark-theme")
     );
     productsInCart.forEach((product) =>
       product.classList.toggle("product-in-cart--dark-theme")
