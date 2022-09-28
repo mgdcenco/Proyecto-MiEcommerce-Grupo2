96e8874c416c8a070df3d0cf40c91040fa9b0b08
DamianCenco <damian.cancela@cencosud.com>
2022-09-27 22:27:27 -0300

    Continuo con dark theme

--- public/bin/dark-mode.js
+++ public/bin/dark-mode.js
@@ -3,7 +3,26 @@ let body = document.querySelector("body");
 let productCards = document.querySelectorAll(".product-list--cards--card");
 let productsInCart = document.querySelectorAll(".product-in-cart--dark-themed");
 
+let theme = localStorage.getItem("Theme");
+window.addEventListener("load", function (e) {
+  //Chequeo el localStorage
+  if (theme === "Dark") {
+    body.classList.toggle("dark-mode--body");
+    productCards.forEach((product) =>
+      product.classList.toggle("product-list--cards--card--dark-theme")
+    );
+    productsInCart.forEach((product) =>
+      product.classList.toggle("product-in-cart--dark-theme")
+    );
+  }
+});
+
 function darkTheme() {
+  //Chequeo el localStorage
+  if (theme === null || theme === "Light") {
+    localStorage.setItem("Theme", "Dark");
+  } else if (theme === "Dark") localStorage.setItem("Theme", "Light");
+  //Seteo las clases
   body.classList.toggle("dark-mode--body");
   productCards.forEach((product) =>
     product.classList.toggle("product-list--cards--card--dark-theme")
